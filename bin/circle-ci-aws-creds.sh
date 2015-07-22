bd37ac9bef1fee1c9546103c648bb85efc62166e
Kurtis Kemple <kurtiskemple@gmail.com>
2015-07-22 02:23:34 -0400

    update aws config

--- bin/circle-ci-aws-creds.sh
+++ bin/circle-ci-aws-creds.sh
@@ -6,5 +6,5 @@ touch ~/.aws/config
 chmod 600 ~/.aws/config
 
 echo "[profile eb-cli]" >> ~/.aws/config
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_KEY" >> ~/.aws/config
+echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/config
+echo "aws_secret_access_key = $AWS_SECRET_KEY" >> ~/.aws/config
