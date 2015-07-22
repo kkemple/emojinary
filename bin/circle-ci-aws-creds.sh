82b2cb45e71e61c52492af85874b18bdfa108724
Kurtis Kemple <kurtiskemple@gmail.com>
2015-07-22 02:38:51 -0400

    update aws config

--- bin/circle-ci-aws-creds.sh
+++ bin/circle-ci-aws-creds.sh
@@ -5,5 +5,8 @@ mkdir ~/.elasticbeanstalk
 touch ~/.elasticbeanstalk/aws_credential_file
 chmod 600 ~/.elasticbeanstalk/aws_credential_file
 
+echo "[default]"
 echo "AWSAccessKeyId=$AWS_ACCESS_KEY_ID" >> ~/.elasticbeanstalk/aws_credential_file
 echo "AWSSecretKey=$AWS_SECRET_KEY" >> ~/.elasticbeanstalk/aws_credential_file
+
+export AWS_CREDENTIAL_FILE=~/.elasticbeanstalk/aws_credential_file
