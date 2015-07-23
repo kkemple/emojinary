639b07046e8c3772bda3a1b28b8775d33bbbb875
Kurtis Kemple <kurtiskemple@gmail.com>
2015-07-23 11:38:27 -0400

    added seed for CI db

---
+++ seed/seed.sql
@@ -0,0 +1,23 @@
+CREATE TABLE team (
+  id                          text PRIMARY KEY,
+  slack_integration_token     text,
+  slack_webhook_url           text,
+);
+
+CREATE TABLE emojinary (
+  id integer PRIMARY KEY DEFAULT nextval('serial')
+  team_id                     text,
+  team_domain                 text,
+  channel_id                  text,
+  channel_name                text,
+  user_id                     text,
+  user_name                   text,
+  text                        text,
+  value                       text,
+  answer                      text,
+  hint                        text,
+  solved                      boolean DEFAULT FALSE,
+  solved_by                   text,
+  created_at                  timestamp,
+  updated_at                  timestamp
+);
