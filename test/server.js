1d329091976c0bd2300d4f397106b5e4f4108b4b
Kurtis Kemple <kurtiskemple@gmail.com>
2015-07-25 17:48:27 -0400

    update back to core functionality after db debugging

--- test/server.js
+++ test/server.js
@@ -6,27 +6,26 @@ const expect = chai.expect;
 
 describe('POST :: /', () => {
   describe('action :: new', () => {
-    it('should return a creation phrase for successfully creating a Emojinary', () => {
-      // server.inject({
-      //   method: 'POST',
-      //   url: '/',
-      //   payload: {
-      //     token: 'test',
-      //     team_id: 'T001',
-      //     team_domain: 'test',
-      //     channel_id: 'C2147483705',
-      //     channel_name: 'test',
-      //     user_id: 'U2147483697',
-      //     user_name: 'kurt',
-      //     command: '/emojinary',
-      //     text: 'new emojinary=[:smile: :camera:] hint=[smile for the camera!] answer=[selfie]'
-      //   }
-      // }, (res) => {
-      //   expect(res.result).to.equal('Emojinary set! :smile:');
-      //   expect(true).to.equal(true);
-      //   done();
-      // });
-      expect(true).to.equal(true);
+    it('should return a creation phrase for successfully creating a Emojinary', (done) => {
+      server.inject({
+        method: 'POST',
+        url: '/',
+        payload: {
+          token: 'test',
+          team_id: 'T001',
+          team_domain: 'test',
+          channel_id: 'C2147483705',
+          channel_name: 'test',
+          user_id: 'U2147483697',
+          user_name: 'kurt',
+          command: '/emojinary',
+          text: 'new emojinary=[:smile: :camera:] hint=[smile for the camera!] answer=[selfie]'
+        }
+      }, (res) => {
+        expect(res.result).to.equal('Emojinary set! :smile:');
+        expect(true).to.equal(true);
+        done();
+      });
     });
   });
 });
